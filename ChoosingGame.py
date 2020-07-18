a8838632fbebff83466eb591913b1ff8ccfb3df8
Vickyyyyyyyyyyyy <39863446+Vickyyyyyyyyyyyy@users.noreply.github.com>
2020-07-18 16:05:28 +0530

    Added the Choosing game

---
+++ ChoosingGame.py
@@ -0,0 +1,27 @@
+import math
+l = 0
+u = int(input("Please Enter an Upper Limit "))
+
+print("Think of a number between 0 and {0}".format(u))
+
+instructions = """1. Enter u if the number selected by you is greater than the guessed number
+2. Enter l if the number selected by you is lesser than the guessed number
+3. Enter c if the number selected by you is equal to the guessed number"""
+count=0
+opt = 'n'
+val=1
+while opt!='c' and opt!='q' and val!=0:
+    if opt=='l':
+        u=val
+    elif opt=='u':
+        l=val
+    else:
+        print("*" * 40)
+        print(instructions)
+        print("*" * 40, "\n")
+    count+=1
+    val = int(math.ceil((l+u)/2))
+    opt = input("Is your number {}? ".format(val))
+    
+if opt=='c':
+    print("Guessed the number in {} turns".format(count))    
--- readme.txt
+++ readme.txt
@@ -1,1 +1,5 @@
-This is the first version of the project that is being used to test the git Functionalities.......
+This is the second version of the project MentalGames. 
+THe release details are as mentioned below:
+v1.0: This only contains the Guessing Game in which the Player guesses the random number selected by the computer by using the hints provided by it.
+
+v2.0: This version is marked by the release of the second game: The Choosing Game in which the computer guesses the random number selected by the player.
